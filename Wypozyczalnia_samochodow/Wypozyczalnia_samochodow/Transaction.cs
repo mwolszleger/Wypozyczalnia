394021eed30b32f3759a7b093b6c84e04ee25b13
mwolszleger <mwolszleger@interia.pl>
2016-06-13 10:47:32 +0200

    brakujacy plik

---
+++ Wypozyczalnia_samochodow/Wypozyczalnia_samochodow/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wypozyczalnia_samochodow
+{
+    class Transaction
+    {
+        public Car car { get; private set; }
+        //private Customer customer {get;private set};
+        private DateTime begin;
+        private DateTime end;
+        //Employee employee1;
+        //Employee employee2;
+        public bool isFinished()
+        {
+            return end!=null;
+        }
+
+    }
+}
